91f869f6db32b1afce5ef2c4b3d33c5cbc6ec505
azabroflovski <azabroflovski@gmail.com>
2026-05-05 15:14:13 +0500

    chore: update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogdbClient.MixProject do
   def project do
     [
       app: :logdb_client,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
