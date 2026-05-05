f01504d4e93ab6f2afff6d8284ee8f08488c21cc
azabroflovski <azabroflovski@gmail.com>
2026-05-05 11:39:38 +0500

    chore: prepare for hex

--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule LogdbClient.MixProject do
   def project do
     [
       app: :logdb_client,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -23,6 +25,18 @@ defmodule LogdbClient.MixProject do
       {:websockex, "~> 0.5.1"},
       {:jason, "~> 1.4"},
       {:req, "~> 0.5.0"}
+    ]
+  end
+
+  defp description() do
+    "Client for Logdb event bus"
+  end
+
+  defp package() do
+    [
+      name: "logdb_client",
+      licenses: ["MIT"],
+      links: %{"GitLab" => "https://gitlab.com/tender.pro/as/logdb_client"}
     ]
   end
 end
